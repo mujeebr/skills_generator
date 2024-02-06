225a2f0c496f6391e3e8fd7e243af5d4d7d5ed9a
mujeebr <58116087+mujeebr@users.noreply.github.com>
2024-02-06 08:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,19 @@
 import streamlit as st
 import google.generativeai as palm
 
-# Configure the Google Palm API
+# Configure the Google Palm API, don't use this api key, use yours.
 palm.configure(api_key="AIzaSyAA28rGYJnOsGasVGEQ-dJRHXqLNTVEQz4")
 
 # Define the Streamlit app
 def main():
     # Set the title and description
-    st.title("Skills and Companies Recommender")
-    st.subheader("Developed by Mujeeb")
-    st.sidebar.image("odinschool1.jpg")
-    st.write("Enter the job role to get recommendations.")
+    st.title("Skills and Companies Recommender") # to set the title for the app
+    st.subheader("Developed by Mujeeb") # it's a sub heading
+    st.sidebar.image("odinschool1.jpg") # to place an image to the left side
+    st.write("Enter the job role to get recommendations.") # suggesting an user what needs to be done with this line of code
 
     # Get user input for job role
-    job_role = st.text_input("Enter a job role:")
+    job_role = st.text_input("Enter a job role:") # input space to enter text
 
     # Define the prompt based on the job role input
     prompt = f'''What are the skills required for a {job_role}? and the top companies to apply to?
